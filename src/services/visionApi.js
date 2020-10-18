02c1b51bb70dcb322100788ffba71976315423ea
WJXHenry <wjxhenrydev@gmail.com>
2020-10-18 02:51:19 -0600

    Download to text file

--- src/services/visionApi.js
+++ src/services/visionApi.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-export function callVisionApi(imageBlobURL) {
+export function callVisionApi(fileName, imageBlobURL) {
   fetch(imageBlobURL).then(res => res.blob()).then(blob => {
     var reader = new FileReader();
     reader.readAsDataURL(blob); 
     reader.onloadend = function() {
         var base64data = reader.result;
         // Remove data attributes
-        getAnnotations(base64data.substr(base64data.indexOf(',') + 1))
+        getAnnotations(fileName, base64data.substr(base64data.indexOf(',') + 1));
     }
   });
 }
 
-function getAnnotations(base64Image) {
+async function getAnnotations(fileName, base64Image) {
   const url = "https://vision.googleapis.com/v1/images:annotate"
   const data = {
     "requests": [
@@ -39,8 +39,17 @@ function getAnnotations(base64Image) {
     data,
     config
   ).then(response => {
-    console.log(response);
+    returnAsFile(fileName, response.data.responses[0].fullTextAnnotation.text);
   }).catch(error => {
     console.log(error);
   });
+}
+
+function returnAsFile(fileName, imageData) {
+  const element = document.createElement("a");
+  const file = new Blob([imageData], {type: 'text/plain;charset=utf-8'});
+  element.href = URL.createObjectURL(file);
+  element.download = fileName + ".txt";
+  document.body.appendChild(element);
+  element.click();
 }
